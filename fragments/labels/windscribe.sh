9a6da583ffee2b90e642495750363192b62de7e9
Sander Schram <macbofh@gmail.com>
2023-01-09 13:39:02 +0100

    Wacom and Windscribe new labels

---
+++ fragments/labels/windscribe.sh
@@ -0,0 +1,12 @@
+windscribe)
+      appTitle="Windscribe"
+      appFiles+=("/Applications/Windscribe.app")
+      appFiles+=("/Library/PrivilegedHelperTools/com.windscribe.helper.macos")
+      appFiles+=("/Library/Logs/com.windscribe.helper.macos")
+      appFiles+=("/Users/$loggedInUser/Library/Application Scripts/com.windscribe.launcher.macos")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/Windscribe")
+      appFiles+=("/Users/$loggedInUser/Library/Containers/com.windscribe.launcher.macos")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.windscribe.Windscribe2.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.windscribe.gui.macos.savedState")
+      appLaunchDaemons+=("/Library/LaunchDaemons/com.windscribe.helper.macos.plist")
+      ;;
