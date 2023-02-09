fc52c2b15e66d4151c7897987911fec400dde5f0
macbofh <macbofh@gmail.com>
2023-02-09 20:15:01 +0100

    added Parallels Desktop & VMWareHorizonclient

---
+++ fragments/labels/parallelsdesktop.sh
@@ -0,0 +1,26 @@
+parallelsdesktop)
+      appTitle="Parallels Desktop"
+      appProcesses+=("Parallels Desktop")
+      appProcesses+=("prl_client_app")
+      appProcesses+=("prl_naptd")
+      appProcesses+=("prl_disp_service")
+      appProcesses+=("watchdog")
+      appFiles+=("/Applications/Parallels Desktop.app")
+      appFiles+=("/Library/Parallels/Parallels Desktop")
+      appFiles+=("/Users/$loggedInUser/Library/Application Scripts/com.parallels.desktop.console.OpenInIE")
+      appFiles+=("/Users/$loggedInUser/Library/Application Scripts/com.parallels.desktop.console.ParallelsMail")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.apple.helpd/Generated/com.parallels.desktop.console.help*18.1.1")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.parallels.desktop.console")
+      appFiles+=("/Users/$loggedInUser/Library/Containers/com.parallels.desktop.console.OpeninIE")
+      appFiles+=("/Users/$loggedInUser/Library/Containers/com.parallels.desktop.console.ParallelsMail")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.parallels.Parallels Desktop Events.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.parallels.Parallels Desktop Statistics.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.parallels.Parallels Desktop.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.parallels.desktop.console.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.parallels.macvm.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.parallels.desktop.console.savedState")
+      appFiles+=("/Users/$loggedInUser/Library/WebKit/com.parallels.desktop.console")
+      preflightCommand+=("kill $(ps aux | grep 'Parallels Desktop.app' | grep watchdog | awk '{print $2}')")
+      ;;
+
+
---
+++ fragments/labels/vmwarehorizonclient.sh
@@ -0,0 +1,10 @@
+vmwarehorizonclient)
+      appTitle="VMware Horizon Client"
+      appFiles+=("/Applications/VMware Horizon Client.app")
+      appFiles+=("/Library/Preferences/com.vmware.horizon.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.vmware.horizon")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.vmware.horizon.keyboard.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.vmware.horizon.plist")
+      appFiles+=("/Users/$loggedInUser/Library/WebKit/com.vmware.horizon")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/VMware Horizon View Client")
+      ;;
