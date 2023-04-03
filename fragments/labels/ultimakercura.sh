fc072cf1a353b48f1a29d5a93501686257a0bb6d
macbofh <macbofh@gmail.com>
2023-04-03 18:43:02 +0200

    Create ultimakercura.sh

---
+++ fragments/labels/ultimakercura.sh
@@ -0,0 +1,8 @@
+ultimakercura)
+      appTitle="Ultimaker Cura"
+      appFiles+=("/Applications/Ultimaker Cura.app")
+      appFiles+=("/Applications/Ultimaker-Cura.app")
+      appFiles+=("/Applications/Ultimaker Cura.localized")
+      appFiles+=("/Applications/Ultimaker Cura-1.localized")
+      appReceipts+=("nl.ultimaker.cura")
+      ;;
