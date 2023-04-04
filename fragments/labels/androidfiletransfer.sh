b152be8ed3a5eb381d0f01bebff21653b3ee1354
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-04-04 22:31:46 +0200

    Create androidfiletransfer.sh
    Uninstaller started - version 2022-12-27 (build: Tue Apr  4 21:15:30 CEST 2023)
    Android File Transfer - Running preflightCommand
    Uninstalling Android File Transfer - LaunchDaemons
    Uninstalling Android File Transfer - LaunchAgents
    Checking for blocking processes...
    Found blocking process Android File Transfer
    Stopping process Android File Transfer
    Found blocking process Android File Transfer Agent
    Stopping process Android File Transfer Agent
    Uninstalling Android File Transfer - Files and Directories
    Removing directory /Applications/Android File Transfer.app...
    Removing file /Users/testuser/Library/Preferences/com.google.android.mtpviewer.pli
    Running Android File Transfer - postflightCommand
    Checking for receipt..
    Uninstaller Finished

---
+++ fragments/labels/androidfiletransfer.sh
@@ -0,0 +1,7 @@
+androidfiletransfer)
+      appTitle="Android File Transfer"
+      appProcesses+=("Android File Transfer")
+      appProcesses+=("Android File Transfer Agent")
+      appFiles+=("/Applications/Android File Transfer.app")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.google.android.mtpviewer.plist")
+      ;;
