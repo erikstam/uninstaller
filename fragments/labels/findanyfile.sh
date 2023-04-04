76c91fb90e86ca951494c9653634aaaacb2ad52d
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-04-04 22:39:02 +0200

    Create findanyfile.sh
    Uninstaller started - version 2022-12-27 (build: Tue Apr  4 21:15:30 CEST 2023)
    Find Any File - Running preflightCommand
    Uninstalling Find Any File - LaunchDaemons
    Uninstalling Find Any File - LaunchAgents
    Checking for blocking processes...
    Found blocking process Find Any File
    Stopping process Find Any File
    Uninstalling Find Any File - Files and Directories
    Removing directory /Applications/Find Any File.app...
    INFO: /Users/testuser/Library/Application Support/Find Any File/FAF.log is not an existing file or folder
    Removing file /Users/testuser/Library/Preferences/org.tempel.findanyfile.plist
    Running Find Any File - postflightCommand
    Checking for receipt..
    Uninstaller Finished

---
+++ fragments/labels/findanyfile.sh
@@ -0,0 +1,7 @@
+findanyfile)
+      appTitle="Find Any File"
+      appProcesses+=("Find Any File")
+      appFiles+=("/Applications/Find Any File.app")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/Find Any File/FAF.log")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/org.tempel.findanyfile.plist")
+;;
