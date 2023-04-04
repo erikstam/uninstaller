fe64e5b4bb6a4e9f356cd92b791d228172b8a2a0
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-04-04 10:36:00 +0200

    Create coderunner.sh
    2023-04-04 10:34:35 Uninstaller started - version 2022-12-27 (build: Mon Apr  3 18:42:08 CEST 2023)
    2023-04-04 10:34:35 CodeRunner - Running preflightCommand
    2023-04-04 10:34:35 Uninstalling CodeRunner - LaunchDaemons
    2023-04-04 10:34:35 Uninstalling CodeRunner - LaunchAgents
    2023-04-04 10:34:35 Checking for blocking processes...
    2023-04-04 10:34:35 Found blocking process CodeRunner
    2023-04-04 10:34:35 Stopping process CodeRunner
    2023-04-04 10:34:38 Uninstalling CodeRunner - Files and Directories
    2023-04-04 10:34:38 Removing directory /Applications/CodeRunner.app...
    2023-04-04 10:34:38 Removing directory /Users/testuser/Library/Application Support/CodeRunner...
    2023-04-04 10:34:38 Removing directory /Users/testuser/Library/Caches/com.krill.CodeRunner...
    2023-04-04 10:34:38 Removing directory /Users/testuser/Library/HTTPStorages/com.krill.CodeRunner...
    2023-04-04 10:34:38 Removing file /Users/testuser/Library/Preferences/com.krill.CodeRunner.plist
    2023-04-04 10:34:38 Removing directory /Users/testuser/Library/Saved Application State/com.krill.CodeRunner.savedState...
    2023-04-04 10:34:38 Running CodeRunner - postflightCommand
    2023-04-04 10:34:38 Checking for receipt..
    2023-04-04 10:34:38 Uninstaller Finished

---
+++ fragments/labels/coderunner.sh
@@ -0,0 +1,10 @@
+coderunner)
+      appTitle="CodeRunner"
+      appProcesses+=("CodeRunner")
+      appFiles+=("/Applications/CodeRunner.app")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/CodeRunner")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.krill.CodeRunner")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.krill.CodeRunner")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.krill.CodeRunner.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.krill.CodeRunner.savedState")
+      ;;
