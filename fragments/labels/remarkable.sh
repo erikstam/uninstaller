7ef49608c14afe693b20b378efb9d4e08a163886
Sander Schram <macbofh@gmail.com>
2023-06-01 15:23:28 +0200

    create remarkable

---
+++ fragments/labels/remarkable.sh
@@ -0,0 +1,9 @@
+remarkable)
+      appTitle="reMarkable"
+      appProcesses+=("reMarkable")
+      appFiles+=("/Applications/reMarkable.app")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.remarkable.desktop.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/remarkable")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.remarkable.desktop")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/remarkable")
+      ;;
