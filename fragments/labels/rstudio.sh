e680fa86e9f940353bf3b9dd05c98c8c91969e91
Sander Schram <macbofh@gmail.com>
2023-06-01 15:56:33 +0200

    rstudio created

---
+++ fragments/labels/rstudio.sh
@@ -0,0 +1,8 @@
+rstudio)
+      appTitle="RStudio"
+      appProcesses+=("RStudio")
+      appFiles+=("/Applications/RStudio.app")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/RStudio")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.rstudio.desktop.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.rstudio.desktop.savedState")
+      ;;
