fa1f185b8fcbf751811a6f52e59851795be655b6
Sander Schram <macbofh@gmail.com>
2023-06-01 15:23:19 +0200

    Create filemaker 16

---
+++ fragments/labels/filemakerpro16.sh
@@ -0,0 +1,12 @@
+filemakerpro16)
+      appTitle="FileMaker Pro"
+      appProcesses+=("FileMaker Pro")
+      appFiles+=("/Applications/Filemaker Pro 16/FileMaker Pro.app")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.filemaker.client.pro12")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.filemaker.client.pro12")      
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.filemaker.client.pro12.binarycookies")         
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.filemaker.client.pro12.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.filemaker.client.pro12.savedState")
+      appFiles+=("/Users/$loggedInUser/Library/Application Support/FileMaker")
+      appFiles+=("/Users/Shared/FileMaker/") 
+      ;;      
