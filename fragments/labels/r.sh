c562a35bc39f1766c3cd66473666d05474e33abd
Sander Schram <macbofh@gmail.com>
2023-06-01 19:21:47 +0200

    R created

---
+++ fragments/labels/r.sh
@@ -0,0 +1,8 @@
+r)
+      appTitle="R"
+      appProcesses+=("R")
+      appFiles+=("/Applications/R.app")
+      appFiles+=("/Library/Frameworks/R.framework")
+      appFiles+=("/opt/R")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/org.R-project.R.plist")
+      ;;
