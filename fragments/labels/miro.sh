63d221ee0ada78e81f341899d6cd02caaad8f8ba
Sander Schram <macbofh@gmail.com>
2023-06-16 04:56:48 +0200

    Create miro.sh

---
+++ fragments/labels/miro.sh
@@ -0,0 +1,10 @@
+miro)
+      appTitle="Miro"
+      appProcesses+=("Miro")
+      appFiles+=("/Applications/Miro.app")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.electron.realtimeboard")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.electron.realtimeboard.ShipIt")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.electron.realtimeboard")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.electron.realtimeboard.plist")
+      appFiles+=("/Users/$loggedInUser/Library/Saved Application State/com.electron.realtimeboard.savedState")
+      ;;
