54b72e07d4f7ac798dc4fb38666db576f20b6123
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-06-27 15:12:36 +0200

    Update lanschoolstudent.sh
    Corrected appTitle. Main App wasn't deleted

--- fragments/labels/lanschoolstudent.sh
+++ fragments/labels/lanschoolstudent.sh
@@ -1,8 +1,9 @@
 lanschoolstudent)
-      appTitle="student"
+      appTitle="LanSchool Student"
       appProcesses+=("student")
       appLaunchAgents+=("/Library/LaunchAgents/com.lanschool.student.plist")
       appLaunchDaemons+=("/Library/LaunchDaemons/com.lanschool.StudentHelper.plist")
+      appFiles+=("/Applications/student.app")
       appFiles+=("/Library/Preferences/com.lanschool.student.config.plist")
       appFiles+=("/Library/Preferences/com.lanschool.student.settings.plist")
       appFiles+=("/Library/PrivilegedHelperTools/com.lanschool.StudentHelper")
