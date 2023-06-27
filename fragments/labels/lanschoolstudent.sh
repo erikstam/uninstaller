a1353b6c6495250396d3144c140ee36bfe0f3d52
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-06-27 14:58:43 +0200

    Create lanschoolstudent.sh
    2023-06-27 14:56:47 Uninstaller started - version 2022-12-27 (build: Thu Jun  1 19:20:19 CEST 2023)
    2023-06-27 14:56:47 student - Running preflightCommand
    2023-06-27 14:56:47 Uninstalling student - LaunchDaemons
    2023-06-27 14:56:47 Removing launchDaemon /Library/LaunchDaemons/com.lanschool.StudentHelper.plist...
    2023-06-27 14:56:47 Uninstalling student - LaunchAgents
    2023-06-27 14:56:47 Removing launchAgent /Library/LaunchAgents/com.lanschool.student.plist...
    2023-06-27 14:56:47 Checking for blocking processes...
    2023-06-27 14:56:47 Found no blocking process...
    2023-06-27 14:56:47 Uninstalling student - Files and Directories
    2023-06-27 14:56:47 Removing file /Library/Preferences/com.lanschool.student.config.plist
    2023-06-27 14:56:47 Removing file /Library/Preferences/com.lanschool.student.settings.plist
    2023-06-27 14:56:47 Removing file /Library/PrivilegedHelperTools/com.lanschool.StudentHelper
    2023-06-27 14:56:47 Removing directory /Users/1111/Library/Caches/com.lanschool.student...
    2023-06-27 14:56:47 Removing directory /Users/1111/Library/HTTPStorages/com.lanschool.student...
    2023-06-27 14:56:47 INFO: /Users/1111/Library/HTTPStorages/com.lanschool.student is not an existing file or folder
    2023-06-27 14:56:47 INFO: /Users/1111/Library/Preferences/com.lanschool.student.plist is not an existing file or folder
    2023-06-27 14:56:47 Running student - postflightCommand
    2023-06-27 14:56:47 Removing student receipts
    Forgot package 'com.lanschool.student.setup.pkg' on '/'.
    2023-06-27 14:56:47 Uninstaller Finished

---
+++ fragments/labels/lanschoolstudent.sh
@@ -0,0 +1,14 @@
+lanschoolstudent)
+      appTitle="student"
+      appProcesses+=("student")
+      appLaunchAgents+=("/Library/LaunchAgents/com.lanschool.student.plist")
+      appLaunchDaemons+=("/Library/LaunchDaemons/com.lanschool.StudentHelper.plist")
+      appFiles+=("/Library/Preferences/com.lanschool.student.config.plist")
+      appFiles+=("/Library/Preferences/com.lanschool.student.settings.plist")
+      appFiles+=("/Library/PrivilegedHelperTools/com.lanschool.StudentHelper")
+      appFiles+=("/Users/$loggedInUser/Library/Caches/com.lanschool.student")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.lanschool.student")
+      appFiles+=("/Users/$loggedInUser/Library/HTTPStorages/com.lanschool.student")
+      appFiles+=("/Users/$loggedInUser/Library/Preferences/com.lanschool.student.plist")
+      appReceipts+=("com.lanschool.student.setup.pkg")
+      ;;
