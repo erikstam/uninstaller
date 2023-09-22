ad7fa3937e95b592a895cb040a98d6964971042f
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2023-09-22 15:08:21 +0200

    Update shottr.sh
    Used the new <<Users>> element option

--- fragments/labels/shottr.sh
+++ fragments/labels/shottr.sh
@@ -1,6 +1,6 @@
 shottr)
       appTitle="Shottr"
       appFiles+=("/Applications/Shottr.app")
-      appFiles+=("/Users/$loggedInUser/Library/Application Scripts/cc.ffitch.shottr")
-      appFiles+=("/Users/$loggedInUser/Library/Containers/cc.ffitch.shottr")
+      appFiles+=("/Users/<<Users>>/Library/Application Scripts/cc.ffitch.shottr")
+      appFiles+=("/Users/<<Users>>/Library/Containers/cc.ffitch.shottr")
       ;;
