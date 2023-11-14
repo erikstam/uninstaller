f6aade043840fcd7485b47c57621e8cfcbdc1fde
Sander Schram <macbofh@gmail.com>
2023-11-14 15:39:17 +0100

    Create Arc and FMP20

---
+++ fragments/labels_disabled/filemakerpro20.sh
@@ -0,0 +1,10 @@
+filemakerpro20)
+      appTitle="FileMaker Pro"
+      appProcesses+=("FileMaker Pro")
+      appFiles+=("/Applications/FileMaker Pro.app")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.filemaker.client.pro12")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.filemaker.client.pro12.binarycookies")      
+      appFiles+=("<<Users>>/Library/Preferences/com.filemaker.client.pro12.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.filemaker.client.pro12.savedState")      
+      appFiles+=("<<Users>>/Library/WebKit/com.filemaker.client.pro12")
+      ;;      
