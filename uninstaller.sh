139f26d40af6edfc0531660f36dd6029faa60969
Sander Schram <macbofh@gmail.com>
2023-11-14 15:40:53 +0100

    New build (14-11-2023)

--- uninstaller.sh
+++ uninstaller.sh
@@ -40,7 +40,7 @@ appBundleIdentifierKey="CFBundleIdentifier"
 
 # Last modification date
 LAST_MOD_DATE="2023-07-30"
-BUILD_DATE="Thu Oct 12 21:12:20 CEST 2023"
+BUILD_DATE="Tue Nov 14 15:40:10 CET 2023"
 
 # MARK: Functions
 
@@ -747,6 +747,21 @@ microsoftremotedesktop)
       appFiles+=("<<Users>>/Library/Group Containers/UBF8T346G9.com.microsoft.oneauth")
       appFiles+=("<<Users>>/Library/Application Scripts/com.microsoft.rdc.macos.qlx")
       appFiles+=("<<Users>>/Library/Application Scripts/com.microsoft.rdc.macos")
+      ;;
+microsoftteamsclassic)
+      appTitle="Microsoft Teams classic"
+      appFiles+=("/Applications/Microsoft Teams classic.app")
+      appFiles+=("/Applications/Microsoft Teams.app")
+      appFiles+=("<<Users>>/Library/WebKit/com.microsoft.teams")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.microsoft.teams.savedState")
+      appFiles+=("<<Users>>/Library/Preferences/com.microsoft.teams.plist ")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.microsoft.teams.binarycookies")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.microsoft.teams")
+      appFiles+=("<<Users>>/Library/Group Containers/UBF8T346G9.com.microsoft.teams")
+      appFiles+=("<<Users>>/Library/Caches/com.microsoft.teams")
+      appFiles+=("/Library/Preferences/com.microsoft.teams.plist")
+      appLaunchDaemons+=("/Library/LaunchDaemons/com.microsoft.teams.TeamsUpdaterDaemon.plist")
+      preflightCommand+=("killall 'Teams'")
       ;;
 microsoftword)
 # Needs more testing
