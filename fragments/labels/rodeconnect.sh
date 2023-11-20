eeed534ab7308daf946ee804055d3b128a031cef
Erik Stam <erik.stam@icloud.com>
2023-11-20 22:12:33 +0100

    Create rodeconnect.sh

---
+++ fragments/labels/rodeconnect.sh
@@ -0,0 +1,14 @@
+rodeconnect)
+      appTitle="RODEConnect"
+      appProcesses+=("RODE Connect ")
+      appProcesses+=("Core Audio Driver (RodeConnect.driver)")
+      appFiles+=("/Applications/RODE Connect.app")
+      appFiles+=("/Library/Audio/Plug-Ins/HAL/RodeConnect.driver")
+      appFiles+=("/Library/Fonts/RODE Noto Sans CJK SC B.otf")
+      appFiles+=("/Library/Fonts/RODE Noto Sans CJK SC R.otf")
+      appFiles+=("/Library/Fonts/RODE Noto Sans Hindi B.ttf")
+      appFiles+=("/Library/Fonts/RODE Noto Sans Hindi R.ttf")
+      appFiles+=("<<Users>>/Library/Application Support/RØDE")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.rode.rodeconnect")
+      appFiles+=("<<Users>>/Library/Caches/com.rode.rodeconnect")
+      ;;
