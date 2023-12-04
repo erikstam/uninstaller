17bf32f17e1375bafbba2036e76bf78087635fa0
Erik Stam <erik.stam@icloud.com>
2023-12-04 10:23:30 +0100

    Create webex.sh

---
+++ fragments/labels/webex.sh
@@ -0,0 +1,10 @@
+webex)
+      appTitle="Webex"
+      appProcesses+=("Webex")
+      appFiles+=("/Applications/Webex.app")
+      appFiles+=("<<Users>>/Library/Application Support/Cisco Spark/Webexteams_upgrades/Webex")
+      appFiles+=("<<Users>>/Library/Application Support/Cisco Spark/Webexteams_upgrades_arm/Webex")
+      appFiles+=("<<Users>>/Library/Caches/Cisco-Systems.Spark")
+      appFiles+=("<<Users>>/Library/Preferences/Cisco-Systems.Spark.plist")
+      appFiles+=("<<Users>>/Library/WebKit/Cisco-Systems.Spark")
+      ;;
