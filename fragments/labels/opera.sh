4334a076143828bb6584ce6e8bed716af0e8baed
Erik Stam <erik.stam@icloud.com>
2023-12-04 10:25:25 +0100

    Initial commit.

---
+++ fragments/labels/opera.sh
@@ -0,0 +1,8 @@
+opera)
+      appTitle="Opera"
+      appProcesses+=("Opera")
+      appFiles+=("/Applications/Opera.app")
+      appFiles+=("<<Users>>/Library/Caches/com.operasoftware.Opera")
+      appFiles+=("<<Users>>/Library/Preferences/com.operasoftware.Opera.plist")
+      appFiles+=("<<Users>>/Library/Application Support/com.operasoftware.Opera")
+      ;;
---
+++ fragments/labels/wireguard.sh
@@ -0,0 +1,9 @@
+wireguard)
+      appTitle="WireGuard"
+      appProcesses+=("WireGuard")
+      appFiles+=("/Applications/WireGuard.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.wireguard.macos")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.wireguard.macos.network-extension")
+      appFiles+=("<<Users>>/Library/Containers/com.wireguard.macos")
+      appFiles+=("<<Users>>/Library/Group Containers/L82V4Y2P3C.group.com.wireguard.macos")
+      ;;
