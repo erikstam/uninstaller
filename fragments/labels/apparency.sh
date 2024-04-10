fc6e16d2ce33e08294e01069ce117494744c8721
P-M Lejon (test-M1) <pme@bonniernews.se>
2024-04-10 14:54:11 +0200

    Correct spelling

--- fragments/labels/apparency.sh
+++ fragments/labels/apparency.sh
@@ -1,6 +1,6 @@
 apparency)
-      appTitle="Apparnecy"
-      appProcesses+=( "Apparnecy" "com.mothersruin.MRSFoundation.UpdateCheckingService" )
+      appTitle="Apparency"
+      appProcesses+=( "Apparency" "com.mothersruin.MRSFoundation.UpdateCheckingService" )
       appFiles+=("/Applications/Apparency.app")
       appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency")
       appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency.QLPreviewExtension")
