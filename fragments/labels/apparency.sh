3a6e5f7547208d54377ab428f40ae0dbf5e9c6d8
P-M Lejon (test-M1) <pme@bonniernews.se>
2024-04-10 14:44:51 +0200

    Add Apparency

---
+++ fragments/labels/apparency.sh
@@ -0,0 +1,7 @@
+apparency)
+      appTitle="Apparnecy"
+      appProcesses+=( "Apparnecy" "com.mothersruin.MRSFoundation.UpdateCheckingService" )
+      appFiles+=("/Applications/Apparency.app")
+      appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency")
+      appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency.QLPreviewExtension")
+      ;;
