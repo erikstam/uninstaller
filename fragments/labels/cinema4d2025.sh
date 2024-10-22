7192f4853f7934e519394017ec93f1b1f9a39901
Sander Schram <macbofh@gmail.com>
2024-10-22 16:42:21 +0200

    Create cinema4d2025.sh

---
+++ fragments/labels/cinema4d2025.sh
@@ -0,0 +1,11 @@
+cinema4d2025)
+      # credit: copy from cinema4d2024 from pmex
+      appTitle="Cinema 4D 2025"
+      appProcesses+=("Cinema 4D")
+      appFiles+=("/Applications/Maxon Cinema 4D 2025")
+      appFiles+=("<<Users>>//Library/Caches/net.maxon.cinema4d")
+      appFiles+=("<<Users>>/Library/HTTPStorages/net.maxon.cinema4d")
+      appFiles+=("<<Users>>/Library/Preferences/Maxon/Maxon Cinema 4D 2024_22E620F3")
+      appFiles+=("<<Users>>/Library/Preferences/net.maxon.cinema4d.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/net.maxon.cinema4d.savedState")
+      ;;
