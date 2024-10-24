f337df7e2327ccd3990df3776ea8b3db7d187cbf
Sander Schram <macbofh@gmail.com>
2024-10-24 08:53:24 +0200

    Update apparency.sh
    Added credit, Application Script folders and 1 additional Container folder

--- fragments/labels/apparency.sh
+++ fragments/labels/apparency.sh
@@ -1,7 +1,12 @@
 apparency)
+      # credit: pmex
       appTitle="Apparency"
       appProcesses+=( "Apparency" "com.mothersruin.MRSFoundation.UpdateCheckingService" )
       appFiles+=("/Applications/Apparency.app")
       appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency")
       appFiles+=("<<Users>>/Library/Containers/com.mothersruin.Apparency.QLPreviewExtension")
+      appFiles+=("<<Users>>/Library/Containers/com.mothersruin.MRSFoundation.UpdateCheckingService")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.mothersruin.Apparency")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.mothersruin.Apparency.QLPreviewExtension")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.mothersruin.MRSFoundation.UpdateCheckingService")
       ;;
