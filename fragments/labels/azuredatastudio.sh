f80d4306d0ba03f89cfe1ee7247ee5206ce20734
Sander Schram <macbofh@gmail.com>
2024-10-27 20:19:39 +0100

    Create azuredatastudio.sh
    Added label for 'Azure Data Studio'

---
+++ fragments/labels/azuredatastudio.sh
@@ -0,0 +1,11 @@
+azuredatastudio)
+      appTitle="Azure Data Studio"      
+      appProcesses+=("Azure Data Studio")
+      appFiles+=("/Applications/Azure Data Studio.app")
+      appFiles+=("<<Users>>/Library/Application Support/azuredatastudio")
+      appFiles+=("<<Users>>/Library/Caches/com.azuredatastudio.oss")
+      appFiles+=("<<Users>>/Library/Caches/com.azuredatastudio.oss.ShipIt")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.azuredatastudio.oss")
+      appFiles+=("<<Users>>/Library/Preferences/com.azuredatastudio.oss.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.azuredatastudio.oss.savedState")
+      ;;
