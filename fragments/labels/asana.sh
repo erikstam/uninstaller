3035ca71583e753c1abcc5eb0f19673ff24ee5b4
Sander Schram <macbofh@gmail.com>
2024-10-27 17:53:15 +0100

    Create asana.sh

---
+++ fragments/labels/asana.sh
@@ -0,0 +1,13 @@
+asana)
+      appTitle="Asana"
+      appProcesses+=("Asana")
+      appFiles+=("/Applications/Asana.app")
+      appFiles+=("<<Users>>/Library/Application Support/Asana")
+      appFiles+=("<<Users>>/Library/Caches/com.electron.asana")
+      appFiles+=("<<Users>>/Library/Caches/com.electron.asana.ShipIt")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.electron.asana")
+      appFiles+=("<<Users>>/Library/Logs/Asana")
+      appFiles+=("<<Users>>/Library/Preferences/com.electron.asana.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.electron.asana.savedState")
+      appReceipts+=("com.electron.asana")
+      ;;
