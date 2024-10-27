8cef465df65dc6ffc34a9d0717990b39b8020cad
Sander Schram <macbofh@gmail.com>
2024-10-27 22:20:01 +0100

    Create keyshot12.sh
    Added Keyshot 12

---
+++ fragments/labels/keyshot12.sh
@@ -0,0 +1,13 @@
+keyshot12)
+      appTitle="KeyShot12"      
+      appProcesses+=("KeyShot" "KeyShot12" "keyshot_daemon" "keyshot")
+      appFiles+=("/Applications/KeyShot12.app")
+      appFiles+=("/Applications/KeyShot12AuthHandler.app")
+      appFiles+=("/Applications/KeyShotCloudHandler.app")
+      appFiles+=("/Library/Application Support/KeyShot12")
+      appFiles+=("<<Users>>/Library/Preferences/com.luxion.Analytics.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.luxion.Crash Reporter.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.luxion.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.luxion.KeyShot 12.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.luxion.Keyshot Updater.plist")
+      ;;
