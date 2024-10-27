6cd3d57ded4e412a285eedd77b6538acda8e3da6
Sander Schram <macbofh@gmail.com>
2024-10-27 22:21:06 +0100

    Create sublimetext.sh
    Added Sublime Text

---
+++ fragments/labels/sublimetext.sh
@@ -0,0 +1,10 @@
+sublimetext)
+      appTitle="Sublime Text"      
+      appProcesses+=("Sublime Text" "sublime_text")
+      appFiles+=("/Applications/Sublime Text.app")
+      appFiles+=("<<Users>>/Library/Application Support/Sublime Text")
+      appFiles+=("<<Users>>/Library/Caches/Sublime Text")
+      appFiles+=("<<Users>>/Library/Caches/com.sublimetext.4")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.sublimetext.4")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.sublimetext.4.savedState")
+      ;;
