c756e1fed5ae961d30154d24ae586cfb03d421aa
Sander Schram <macbofh@gmail.com>
2024-10-30 17:38:16 +0100

    Create virtualbuddy.sh

---
+++ fragments/labels/virtualbuddy.sh
@@ -0,0 +1,10 @@
+virtualbuddy)
+      appTitle="VirtualBuddy"      
+      appProcesses+=("VirtualBuddy")
+      appFiles+=("/Applications/VirtualBuddy.app")
+      appFiles+=("<<Users>>/Library/Application Support/VirtualBuddy")
+      appFiles+=("<<Users>>/Library/Caches/codes.rambo.VirtualBuddy")
+      appFiles+=("<<Users>>/Library/HTTPStorages/codes.rambo.VirtualBuddy")
+      appFiles+=("<<Users>>/Library/Preferences/codes.rambo.VirtualBuddy.plist")
+      appFiles+=("<<Users>>/Library/WebKit/codes.rambo.VirtualBuddy")
+      ;;
