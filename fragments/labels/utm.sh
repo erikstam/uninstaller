9a4626c5e1300e64d6b264b33c429d137540831c
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2024-11-14 21:59:59 +0100

    Update utm.sh

--- fragments/labels/utm.sh
+++ fragments/labels/utm.sh
@@ -1,10 +1,13 @@
 utm)
-# If you also want to remove the VM's. Uncomment the lines
 	appTitle="UTM"      
 	appProcesses+=("UTM")
 	appFiles+=("/Applications/UTM.app")
-#	appFiles+=("<<Users>>/Library/Application Scripts/com.utmapp.UTM")
-#	appFiles+=("<<Users>>/Library/Containers/com.utmapp.UTM")
-#	appFiles+=("<<Users>>/Library/Group Containers/WDNLXAD4W8.com.utmapp.UTM")
-#	appFiles+=("<<Users>>/Library/Saved Application State/com.utmapp.UTM.savedState")
+	appFiles+=("<<Users>>/Library/Saved Application State/com.utmapp.UTM.savedState")
+	appFiles+=("<<Users>>/Library/Application Scripts/com.utmapp.UTM")
+	printlog "IMPORTANT: We don't want to touch User data! Keeping the VM's intact..."
+	printlog "IMPORTANT: Check label for extra info."
+	# If you want to remove the VM's, they live here:
+	# /Users/homefolder/Library/Containers/com.utmapp.UTM
+	# /Users/homefolder/Library/Group Containers/WDNLXAD4W8.com.utmapp.UTM
 	;;
+	
