87bf6ee9cd4a58c0d94ee75b0a80f2e5c1691d57
Sander Schram <macbofh@gmail.com>
2024-11-14 19:52:02 +0100

    Create iina.sh

---
+++ fragments/labels/iina.sh
@@ -0,0 +1,12 @@
+iina)
+      appTitle="IINA"
+      appFiles+=("/Applications/IINA.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.colliderli.iina.OpenInIINA")
+      appFiles+=("<<Users>>/Library/Application Support/com.colliderli.iina")
+      appFiles+=("<<Users>>/Library/Caches/com.colliderli.iina")
+      appFiles+=("<<Users>>/Library/Containers/com.colliderli.iina.OpenInIINA")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.colliderli.iina")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.colliderli.iina.binarycookies")
+      appFiles+=("<<Users>>/Library/Preferences/com.colliderli.iina.plist")
+      appFiles+=("<<Users>>/Library/WebKit/com.colliderli.iina")
+      ;;
