6284132aa9888bcbb41e855d9a3531f889c8fb42
Erik Stam <erik.stam@icloud.com>
2024-11-15 08:58:29 +0100

    Create imazingprofileeditor.sh

---
+++ fragments/labels/imazingprofileeditor.sh
@@ -0,0 +1,14 @@
+imazingprofileeditor)
+      appTitle="iMazing Profile Editor"
+      appFiles+=("/Applications/iMazing Profile Editor.app")
+      appFiles+=("<<Users>>/Library/Containers/com.DigiDNA.iMazingProfileEditorMac")
+      appFiles+=("<<Users>>/Library/Preferences/com.DigiDNA.iMazing2Mac.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.DigiDNA.iMazingProfileEditorMac.plist")
+      appFiles+=("<<Users>>/Library/WebKit/com.DigiDNA.iMazing2Mac")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.DigiDNA.iMazing2Mac")
+      appFiles+=("<<Users>>/Library/Application Support/com.DigiDNA.iMazing2Mac")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.DigiDNA.iMazingProfileEditorMac")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.DigiDNA.iMazingProfileEditorMac")
+      appFiles+=("<<Users>>/Library/Application Support/iMazing")
+      ;;
+      
