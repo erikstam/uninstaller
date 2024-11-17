c5d7faa24f7d13680b0ca6dd8e26c26831481ef7
Sander Schram <macbofh@gmail.com>
2024-11-17 14:01:10 +0100

    removed duplicate line

--- fragments/labels/setupmanager.sh
+++ fragments/labels/setupmanager.sh
@@ -8,12 +8,10 @@ setupmanager)
       appFiles+=("<<Users>>/Library/Caches/com.jamf.setupmanager")
       appFiles+=("<<Users>>/Library/HTTPStorages/com.jamf.setupmanager")
       appFiles+=("<<Users>>/Library/HTTPStorages/com.jamf.setupmanager.binarycookies")
-      appFiles+=("<<Users>>/Library/Caches/com.jamf.setupmanager")
       # as Setup Manager runs at enrollment or at the login window it will store its files
       # in the root directory
       appFiles+=("/private/var/root/Library/Caches/com.jamf.setupmanager")
       appFiles+=("/private/var/root/Library/HTTPStorages/com.jamf.setupmanager")
       appFiles+=("/private/var/root/Library/HTTPStorages/com.jamf.setupmanager.binarycookies")
-      appFiles+=("/private/var/root/Library/Caches/com.jamf.setupmanager")
       appReceipts+=("com.jamf.setupmanager")
       ;;
