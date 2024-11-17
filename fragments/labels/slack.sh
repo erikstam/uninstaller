ca7a6d77e0a6a828825760744466020b548ba07e
Sander Schram <macbofh@gmail.com>
2024-11-17 14:01:13 +0100

    Create slack.sh

---
+++ fragments/labels/slack.sh
@@ -0,0 +1,9 @@
+slack)
+      appTitle="Slack"
+      appProcesses+=("Slack")
+      appFiles+=("/Applications/Slack.app")
+      appFiles+=("<<Users>>/Library/Application Support/Slack")
+      appFiles+=("<<Users>>/Library/Preferences/com.tinyspeck.slackmacgap.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.tinyspeck.slackmacgap.savedState")
+      appReceipts+=("com.tinyspeck.slackmacgap")
+      ;;
