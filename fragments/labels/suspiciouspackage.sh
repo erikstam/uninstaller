4108105146196e3ddb3c937260f056654ab26b07
Sander Schram <macbofh@gmail.com>
2024-11-17 14:01:15 +0100

    Create suspiciouspackage.sh

---
+++ fragments/labels/suspiciouspackage.sh
@@ -0,0 +1,8 @@
+suspiciouspackage)
+      appTitle="Suspicious Package"
+      appProcesses+=("Suspicious Package")
+      appFiles+=("/Applications/Suspicious Package.app")
+      appFiles+=("<<Users>>/Library/Preferences/com.mothersruin.SuspiciousPackage.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.mothersruin.SuspiciousPackageApp.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.mothersruin.SuspiciousPackageApp.savedState")
+      ;;
