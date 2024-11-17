c5788f0218c6714f83e9c5ad5552128d58977056
Sander Schram <macbofh@gmail.com>
2024-11-17 14:00:35 +0100

    Create iterm2.sh

---
+++ fragments/labels/iterm2.sh
@@ -0,0 +1,7 @@
+iterm2)
+      appTitle="iTerm2"
+      appProcesses+=("iTerm2")
+      appFiles+=("/Applications/iTerm.app")
+      appFiles+=("<<Users>>/Library/Application Support/iTerm2")
+      appFiles+=("<<Users>>/Library/Preferences/com.googlecode.iterm2.plist")
+      ;;
