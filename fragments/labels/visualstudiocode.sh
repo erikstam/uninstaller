949c9a379b933c0365688dd4ce1f634e54118414
Sander Schram <macbofh@gmail.com>
2024-11-18 10:12:53 +0100

    workaround for killing Electon

--- fragments/labels/visualstudiocode.sh
+++ fragments/labels/visualstudiocode.sh
@@ -7,4 +7,5 @@ visualstudiocode)
       appFiles+=("<<Users>>/Library/Preferences/com.microsoft.VSCode.plist")
       appFiles+=("<<Users>>/Library/Saved Application State/com.microsoft.VSCode.savedState")
       appFiles+=("<<Users>>/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.vscode.sfl2")
+      preflightCommand+=("kill -9 $(pgrep -f /Applications/Visual\ Studio\ Code.app/Contents/MacOS/Electron)")
       ;;
