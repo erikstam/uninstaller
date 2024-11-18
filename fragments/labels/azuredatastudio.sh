f363752adbcd6b2549d01527cb1c4be006678868
Sander Schram <macbofh@gmail.com>
2024-11-18 10:10:52 +0100

    workaround for Electron process

--- fragments/labels/azuredatastudio.sh
+++ fragments/labels/azuredatastudio.sh
@@ -1,5 +1,7 @@
 azuredatastudio)
-      appTitle="Azure Data Studio"      
+      appTitle="Azure Data Studio"
+      # Azure Data Studio runs as process Electron. Killing every Electron may be to dangerous
+      # so i'm using a preflightCommand for that
       appProcesses+=("Azure Data Studio")
       appFiles+=("/Applications/Azure Data Studio.app")
       appFiles+=("<<Users>>/Library/Application Support/azuredatastudio")
@@ -8,4 +10,5 @@ azuredatastudio)
       appFiles+=("<<Users>>/Library/HTTPStorages/com.azuredatastudio.oss")
       appFiles+=("<<Users>>/Library/Preferences/com.azuredatastudio.oss.plist")
       appFiles+=("<<Users>>/Library/Saved Application State/com.azuredatastudio.oss.savedState")
+      preflightCommand+=("kill -9 $(pgrep -f /Applications/Azure\ Data\ Studio.app/Contents/MacOS/Electron)")
       ;;
