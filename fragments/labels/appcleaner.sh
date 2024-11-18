b00455f70aed5b595950fea0af932a824405d8ec
Sander Schram <macbofh@gmail.com>
2024-11-18 10:10:29 +0100

    Create appcleaner.sh

---
+++ fragments/labels/appcleaner.sh
@@ -0,0 +1,8 @@
+appcleaner)
+      appTitle="AppCleaner"
+      appProcesses+=("AppCleaner")
+      appFiles+=("/Applications/AppCleaner.app")
+      appFiles+=("<<Users>>/Library/HTTPStorages/net.freemacsoft.AppCleaner")
+      appFiles+=("<<Users>>/Library/Preferences/net.freemacsoft.AppCleaner.plist")
+      appFiles+=("<<Users>>/Library/Saved Application State/net.freemacsoft.AppCleaner.savedState")
+      ;;
