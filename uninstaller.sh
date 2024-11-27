56aacaaaa2ac94ae3d85d4c865784950c33e6154
P-M Lejon <15982689+pmex@users.noreply.github.com>
2024-11-27 11:21:58 +0100

    Editing the right file is crucial

--- uninstaller.sh
+++ uninstaller.sh
@@ -1407,7 +1407,6 @@ sourcetree)
 spotify)
       appTitle="Spotify"
       appFiles+=("/Applications/Spotify.app")
-      appFiles+=("<<Users>>/Applications/Spotify.app")
       appFiles+=("<<Users>>/Library/Application Support/Spotify/")
       appFiles+=("<<Users>>/Library/HTTPStorages/com.spotify.client")
       appFiles+=("<<Users>>/Library/Preferences/com.spotify.client.plist")
