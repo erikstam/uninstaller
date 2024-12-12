9c9999219bea0920bdcbda6c5b3edff47b1d66f6
Sander Schram <macbofh@gmail.com>
2024-12-12 19:49:18 +0100

    Create elgatostreamdeck.sh

---
+++ fragments/labels/elgatostreamdeck.sh
@@ -0,0 +1,9 @@
+elgatostreamdeck)
+      appTitle="Elgato Stream Deck"
+      appFiles+=("/Applications/Elgato Stream Deck.app")
+      appProcesses+=("Stream Deck")
+      appFiles+=("<<Users>>/Library/Application Support/com.elgato.StreamDeck")
+      appFiles+=("<<Users>>/Library/LaunchAgents/com.elgato.StreamDeck.plist")
+      appFiles+=("<<Users>>/Library/Logs/ElgatoStreamDeck")
+      appReceipts+=("com.elgato.StreamDeck")
+      ;;
