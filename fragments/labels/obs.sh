47ea99868a743095277ff3b00ce0ebf713e1e38b
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-01-02 16:36:26 +0100

    Add OBS Studio (obs)

---
+++ fragments/labels/obs.sh
@@ -0,0 +1,8 @@
+obs)
+      appTitle="OBS Studio"
+      appProcesses+=("OBS Studio")
+      appFiles+=("/Applications/OBS.app")
+      appFiles+=("<<Users>>/Library/Preferences//Users/com.obsproject.obs-studio.plist")
+      appFiles+=("<<Users>>/Library/Application Support/obs-studio")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.obsproject.obs-studio.savedState")
+      ;;
