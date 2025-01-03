a1a6a26927719366638fc099f3bb70e480c37842
Sander Schram <macbofh@gmail.com>
2025-01-03 14:39:15 +0100

    Create dfublaster.sh

---
+++ fragments/labels/dfublaster.sh
@@ -0,0 +1,8 @@
+dfublaster)
+      appTitle="DFU Blaster"
+      appProcesses+=("DFU Blaster")
+      appFiles+=("/Applications/DFU Blaster.app")
+      appFiles+=("/Library/LaunchDaemons/com.twocanoes.dfublasterhelper.plist")
+      appFiles+=("/Library/PrivilegedHelperTools/com.twocanoes.dfublasterhelper")
+      appFiles+=("<<Users>>/Library/Preferences/com.twocanoes.DFU-Blaster.plist")
+      ;;
