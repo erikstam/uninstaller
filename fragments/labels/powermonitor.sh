7a24e3cc8e9fbd27da6d49d374191a3b022a80ab
Sander Schram <macbofh@gmail.com>
2025-01-03 14:39:20 +0100

    Create powermonitor.sh

---
+++ fragments/labels/powermonitor.sh
@@ -0,0 +1,10 @@
+powermonitor)
+      appTitle="Power Monitor"
+      appProcesses+=("Power Monitor")
+      appFiles+=("/Applications/Power Monitor.app")
+      appFiles+=("/Library/LaunchDaemons/corp.sap.PowerMonitorDaemon.plist")
+      appFiles+=("/Users/Shared/Power Monitor")
+      appFiles+=("<<Users>>/Library/Application Scripts/corp.sap.PowerMonitor")
+      appFiles+=("<<Users>>/Library/Containers/corp.sap.PowerMonitor")
+      appReceipts+=("corp.sap.PowerMonitor.pkg")
+      ;;
