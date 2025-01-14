304c7cebca7369ec9be2ef9bb4857fd1efb845b9
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-01-14 13:23:49 +0100

    Add Hype 4

---
+++ fragments/labels/hype4.sh
@@ -0,0 +1,8 @@
+hype4)
+      appTitle="Hype 4"
+      appProcesses+=("Hype4" "com.tumult.Hype")
+      appFiles+=("/Applications/Hype 4.app")
+      appFiles+=("<<Users>>/Library/Preferences/com.tumult.Hype4.plist")
+      appFiles+=("<<Users>>/Library/Containers/Hype 4")
+      appFiles+=("<<Users>>/Library/Containers/com.tumult.HypeThumbnailGenerator")
+      ;;
