f7fefe5f1591502f43ad9cab065c3d655884353c
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-01-21 09:46:52 +0100

    added "soffice" to appProcesses

--- fragments/labels/libreoffice.sh
+++ fragments/labels/libreoffice.sh
@@ -1,6 +1,6 @@
 libreoffice)
       appTitle="LibreOffice"
-      appProcesses+=("LibreOffice")
+      appProcesses+=("LibreOffice" "soffice")
       appFiles+=("/Applications/LibreOffice.app")
       appFiles+=("<<Users>>/Library/Application Support/LibreOffice")
       appFiles+=("<<Users>>/Saved Application State/org.libreoffice.script.savedState")
