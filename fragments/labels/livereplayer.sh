1aa86629e4e6298c3f0274b009a2a827e5d9de11
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-01-23 11:28:22 +0100

    Add LiveReplayer

---
+++ fragments/labels/livereplayer.sh
@@ -0,0 +1,7 @@
+livereplayer)
+      appTitle="LiveReplayer"
+      appProcesses+=("LiveReplayer")
+      appFiles+=("/Applications/LiveReplayer.app")
+      appFiles+=("<<Users>>/Library/Application Support/com.fsm.mac.LiveReplayer")
+      appFiles+=("<<Users>>/Library/Preferences/com.fsm.mac.LiveReplayer.plist")
+      ;;
