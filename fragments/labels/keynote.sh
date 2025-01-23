5d5fdbd86181fdad1930dd6c11535ae2d8c2e7cc
Sander Schram <macbofh@gmail.com>
2025-01-23 19:45:29 +0100

    Create keynote.sh

---
+++ fragments/labels/keynote.sh
@@ -0,0 +1,12 @@
+keynote)
+      appTitle="Keynote"
+      appProcesses+=("Keynote")
+      appFiles+=("/Applications/Keynote.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.apple.iWork.Keynote")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.apple.iWork.Keynote.KeynoteNotificationServiceExtension")
+      appFiles+=("<<Users>>/Library/Caches/com.apple.helpd/Generated/com.apple.iWork.Keynote.remote.help*14.3")
+      appFiles+=("<<Users>>/Library/Containers/com.apple.iWork.Keynote")
+      appFiles+=("<<Users>>/Library/Containers/com.apple.iWork.Keynote.KeynoteNotificationServiceExtension")
+      appFiles+=("<<Users>>/Library/Group Containers/74J34U3R6X.com.apple.iWork")
+      appFiles+=("<<Users>>/Library/Saved Application State/com.apple.iWork.Keynote.savedState")
+      ;;
