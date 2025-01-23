19064d49e5ce9e91201c740871d4c73fbc7a76f5
Sander Schram <macbofh@gmail.com>
2025-01-23 19:46:01 +0100

    Additional appFiles in userspace

--- fragments/labels/imovie.sh
+++ fragments/labels/imovie.sh
@@ -2,6 +2,9 @@ imovie)
       appTitle="iMovie"
       appProcesses+=("iMovie")
       appFiles+=("/Applications/iMovie.app")
-      appFiles+=("<<Users>>/Library/Containers/com.apple.iMovieApp")
       appFiles+=("<<Users>>/Library/Application Scripts/com.apple.iMovieApp")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.apple.videoapps.OOPProResRawService")
+      appFiles+=("<<Users>>/Library/Application Support/iMovie")
+      appFiles+=("<<Users>>/Library/Containers/com.apple.iMovieApp")
+      appFiles+=("<<Users>>/Library/Containers/com.apple.videoapps.OOPProResRawService")
       ;;
