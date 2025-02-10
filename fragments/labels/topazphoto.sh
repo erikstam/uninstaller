8669661de8322b841de39197606662ffe3892a01
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-02-10 11:24:28 +0100

    Add Topaz Photo AI

---
+++ fragments/labels/topazphoto.sh
@@ -0,0 +1,23 @@
+topazphoto|\
+topazphotoai)
+      appTitle="Topaz Photo AI.app"
+      appFiles+=("/Applications/Topaz Photo AI.app")
+      appFiles+=("/Applications/Adobe Photoshop 2024/Plug-ins/TopazPhotoAI.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2024/Plug-ins/TopazPhotoAIApply.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2024/Plug-ins/TopazPhotoAIAutomate.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2024/Plug-ins/TopazPhotoAIGather.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2025/Plug-ins/TopazPhotoAI.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2025/Plug-ins/TopazPhotoAIApply.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2025/Plug-ins/TopazPhotoAIAutomate.plugin")
+      appFiles+=("/Applications/Adobe Photoshop 2025/Plug-ins/TopazPhotoAIGather.plugin")
+      appFiles+=("/Library/Application Support/Adobe/Plug-Ins/CC/TopazPhotoAI.plugin")
+      appFiles+=("/Library/Application Support/Adobe/Plug-Ins/CC/TopazPhotoAIApply.plugin")
+      appFiles+=("/Library/Application Support/Adobe/Plug-Ins/CC/TopazPhotoAIAutomate.plugin")
+      appFiles+=("/Library/Application Support/Adobe/Plug-Ins/CC/TopazPhotoAIGather.plugin")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.topazlabs.TopazPhotoAIplugin")
+      appFiles+=("<<Users>>/Library/Application Support/Topaz Labs LLC/Topaz Photo AI")
+      appFiles+=("<<Users>>/Library/Application Support/Adobe/Lightroom/Modules/Topaz Photo AI.lrplugin")
+      appFiles+=("<<Users>>/Library/Application Support/Adobe/Lightroom/External Editor Presets/TopazPhotoAI.lrtemplate")
+      appFiles+=("<<Users>>/Library/Application Support/Affinity Photo/Plugins/TopazPhotoAI.plugin")
+      appReceipts+=("com.topazlabs.TopazPhotoAI")
+      ;;
