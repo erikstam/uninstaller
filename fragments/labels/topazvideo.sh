6192bf49ce9dbe087d7330f3aaeb3dfa28c60dd0
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-02-10 12:45:59 +0100

    Add Topaz Video AI

---
+++ fragments/labels/topazvideo.sh
@@ -0,0 +1,7 @@
+topazvideo|\
+topazvideoai)
+      appTitle="Topaz Video AI.app"
+      appFiles+=("/Applications/Topaz Video AI.app")
+      appFiles+=("<<Users>>/Library/Application Support/Topaz Labs LLC/Topaz Video AI")
+      appReceipts+=("com.topazlabs.VAIPackage")
+      ;;
