f68cca1b148f41a11467a78e2a97a8583d5f4f36
MacMike077 <104210875+MacMike077@users.noreply.github.com>
2025-03-05 11:46:27 +0100

    Create logioptions.sh
    Script result: 2025-03-05 11:34:55 Uninstaller started - build 2025-03-05
    2025-03-05 11:34:55 Logi Options - Running preflightCommand
    2025-03-05 11:34:55 Uninstalling Logi Options - LaunchDaemons
    2025-03-05 11:34:55 Uninstalling Logi Options - LaunchAgents
    2025-03-05 11:34:56 unloading /Library/LaunchAgents/com.logitech.manager.daemon.plist for testuser
    2025-03-05 11:34:56 Removing launchAgent /Library/LaunchAgents/com.logitech.manager.daemon.plist...
    2025-03-05 11:34:56 Checking for blocking processes...
    2025-03-05 11:34:56 Found blocking process Logi Options
    2025-03-05 11:34:56 Stopping process Logi Options
    2025-03-05 11:34:59 Uninstalling Logi Options - Files and Directories
    2025-03-05 11:34:59 Removing directory /Applications/Logi Options.app...
    2025-03-05 11:35:00 Removing directory /Users/Shared/logi...
    2025-03-05 11:35:00 Removing directory /Library/Application Support/Logi...
    2025-03-05 11:35:00 Removing directory /Library/Application Support/Logitech.localized/Logitech Options.localized...
    2025-03-05 11:35:00 Removing directory /Users/testuser/Library/Application Support/Logitech/Options...
    2025-03-05 11:35:01 Removing directory /Users/testuser/Library/Application Support/com.logitech.logiaipromptbuilder...
    2025-03-05 11:35:01 Running Logi Options - postflightCommand
    2025-03-05 11:35:01 Checking for receipt..
    2025-03-05 11:35:01 Removing Logi Options receipts
    Forgot package 'com.logitech.manager.pkg' on '/'.
    2025-03-05 11:35:01 Uninstaller Finished

---
+++ fragments/labels/logioptions.sh
@@ -0,0 +1,12 @@
+logioptions)
+      appTitle="Logi Options"
+      appFiles+=("/Applications/Logi Options.app")
+      appFiles+=("/Users/Shared/logi")
+      appFiles+=("/Library/Application Support/Logi")
+      appFiles+=("/Library/Application Support/Logitech.localized/Logitech Options.localized")
+      appFiles+=("<<Users>>/Library/Application Support/Logitech/Options")
+      appFiles+=("<<Users>>/Library/Application Support/com.logitech.logiaipromptbuilder")
+      appFiles+=("/Library/LaunchAgents/com.logitech.manager.daemon.plist")
+      appLaunchAgents+=("/Library/LaunchAgents/com.logitech.manager.daemon.plist")
+      appReceipts+=("com.logitech.manager.pkg")
+      ;;
