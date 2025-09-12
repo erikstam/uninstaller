710e2220043c40f4cec728228210c6761db24bdb
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-09-12 10:19:05 +0200

    Corrected folder path

--- fragments/labels/cinema4d2026.sh
+++ fragments/labels/cinema4d2026.sh
@@ -4,7 +4,7 @@ cinema4d2026)
       appFiles+=("/Applications/Maxon Cinema 4D 2026")
       appFiles+=("<<Users>>//Library/Caches/net.maxon.cinema4d")
       appFiles+=("<<Users>>/Library/HTTPStorages/net.maxon.cinema4d")
-      appFiles+=("<<Users>>/Library/Preferences/Maxon/Maxon Cinema 4D 2026_FFA38A4B")
+      appFiles+=("<<Users>>/Library/Preferences/Maxon/Maxon Cinema 4D 2026_9D810372")
       appFiles+=("<<Users>>/Library/Preferences/net.maxon.cinema4d.plist")
       appFiles+=("<<Users>>/Library/Saved Application State/net.maxon.cinema4d.savedState")
       ;;
