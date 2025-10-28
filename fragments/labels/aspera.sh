c18d9a2468c9147d55b44781ee7e58fa2fc4ef00
P-M Lejon <15982689+pmex@users.noreply.github.com>
2025-10-28 13:14:19 +0100

    Add aspera (IBM Aspera)

---
+++ fragments/labels/aspera.sh
@@ -0,0 +1,13 @@
+aspera|\
+ibmaspera)
+      appTitle="IBM Aspera"
+      appProcesses+=("aspera-desktop")
+      appFiles+=("/Applications/IBM Aspera.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.ibm.software.aspera.desktop.safari-extension")
+      appFiles+=("<<Users>>/Library/Application Support/com.ibm.software.aspera.desktop")
+      appFiles+=("<<Users>>/Library/Caches/com.ibm.software.aspera.desktop")
+      appFiles+=("<<Users>>/Library/Logs/com.ibm.software.aspera.desktop")
+      appFiles+=("<<Users>>/Library/Logs/Aspera")
+      appFiles+=("<<Users>>/Library/WebKit/com.ibm.software.aspera.desktop")
+      appReceipts+=("com.ibm.software.aspera.desktop")
+      ;;
