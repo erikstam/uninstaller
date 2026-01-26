b0e97e148a65cacd92740184a7ba6aebcc511016
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-01-26 13:52:32 +0100

    FireCut for DaVinci Resolve

---
+++ fragments/labels/firecutfordavinciresolve.sh
@@ -0,0 +1,6 @@
+firecutfordavinciresolve)
+      appTitle="FireCut for DaVinci Resolve"
+      appProcesses+=("Resolve")
+      appFiles+=("/Applications/FireCut for DaVinci Resolve.app")
+      appFiles+=("<<Users>>/Library/Application Support/FireCut for DaVinci Resolve")
+      ;;
