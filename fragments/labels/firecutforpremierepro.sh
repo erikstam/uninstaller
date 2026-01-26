99e0813b3946fb48e68cf0e1fe88f34a52ccf1f6
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-01-26 13:53:16 +0100

    Add FireCut for Premiere Pro

---
+++ fragments/labels/firecutforpremierepro.sh
@@ -0,0 +1,10 @@
+firecutforpremierepro)
+      appTitle="FireCut for Premiere Pro"
+      appProcesses+=( "Adobe Premiere Pro 2024" )
+	  appProcesses+=( "Adobe Premiere Pro 2025" )
+	  appProcesses+=( "Adobe Premiere Pro 2026" )
+	  appProcesses+=( "Adobe Premiere Pro 2027" )
+      appFiles+=("/Library/Application Support/Adobe/CEP/extensions/com.firecut.base")
+      appFiles+=("<<Users>>/Library/Application Support/Adobe/CEP/extensions/com.firecut.base")
+      appFiles+=("<<Users>>/Library/Application Support/FireCut")
+      ;;
