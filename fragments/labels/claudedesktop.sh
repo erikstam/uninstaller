f127d5512f6ba8c8144ef26cae8e363cd2b46e5b
Sander Schram <macbofh@gmail.com>
2026-04-03 13:47:17 +0200

    Create claudedesktop.sh

---
+++ fragments/labels/claudedesktop.sh
@@ -0,0 +1,12 @@
+claudedesktop)
+    appTitle="Claude"
+    appProcesses+=("Claude")
+    appFiles+=("/Applications/Claude.app")
+    appFiles+=("<<Users>>/Library/Application Support/Claude")
+    appFiles+=("<<Users>>/Library/Caches/com.anthropic.claudefordesktop")
+    appFiles+=("<<Users>>/Library/Caches/com.anthropic.claudefordesktop.ShipIt")
+    appFiles+=("<<Users>>/Library/HTTPStorages/com.anthropic.claudefordesktop")
+    appFiles+=("<<Users>>/Library/Logs/Claude")
+    appFiles+=("<<Users>>/Library/Preferences/com.anthropic.claudefordesktop.plist")
+    appReceipts+=("com.anthropic.claudefordesktop")
+    ;;
