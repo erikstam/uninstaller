a087301d4761ec6feb8f0c2353d88fb761b35984
Sander Schram <macbofh@gmail.com>
2026-04-03 13:47:05 +0200

    Update superman.sh
    preflight command used in Super-Friends

--- fragments/labels/superman.sh
+++ fragments/labels/superman.sh
@@ -5,5 +5,5 @@ superman)
       appFiles+=("/usr/local/bin/super")
       appFiles+=("/var/run/super.pid")
       appLaunchDaemons+=("/Library/LaunchDaemons/com.macjutsu.super.plist")
-      preflightCommand+=("/Library/Management/super/super --reset-super --auth-delete-all")
+      preflightCommand+=("/Library/Management/super/super --reset-super --workflow-disable-update-check --workflow-disable-relaunch --auth-delete-all")
       ;;
