dfe008b7052dfe7d15ab71e626b62759c5263f0b
Sander Schram <macbofh@gmail.com>
2026-04-03 15:47:15 +0200

    Create telegram.sh

---
+++ fragments/labels/telegram.sh
@@ -0,0 +1,11 @@
+telegram)
+      appTitle="Telegram"
+      appFiles+=("/Applications/Telegram.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/ru.keepcoder.Telegram")
+      appFiles+=("<<Users>>/Library/Application Scripts/ru.keepcoder.Telegram.TelegramShare")
+      appFiles+=("<<Users>>/Library/Containers/ru.keepcoder.Telegram")
+      appFiles+=("<<Users>>/Library/Containers/ru.keepcoder.TelegramShare")
+      appFiles+=("<<Users>>/Library/Group Containers/6N38VWS5BX.ru.keepcoder.Telegram")
+      appFiles+=("<<Users>>/Library/Group Containers/6N38VWS5BX.ru.keepcoder.TelegramShare")  
+      appReceipts+=("ru.keepcoder.Telegram")
+      ;;
