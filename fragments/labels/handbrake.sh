07a5b9e950504bd497af28a36c8984ae5eba46c3
Sander Schram <macbofh@gmail.com>
2026-05-01 11:03:14 +0200

    New Handbrake label

---
+++ fragments/labels/handbrake.sh
@@ -0,0 +1,12 @@
+handbrake)
+      appTitle="Handbrake"
+      appProcesses+=("Handbrake")
+      appFiles+=("/Applications/HandBrake.app")
+      appFiles+=("<<Users>>/Library/Application Scripts/fr.handbrake.HandBrake")
+      appFiles+=("<<Users>>/Library/Application Scripts/fr.handbrake.HandBrakeXPCService")
+      appFiles+=("<<Users>>/Library/Application Scripts/fr.handbrake.sparkle-project.DownloaderService")
+      appFiles+=("<<Users>>/Library/Caches/fr.handbrake.HandBrake")
+      appFiles+=("<<Users>>/Library/Containers/fr.handbrake.HandBrake")
+      appFiles+=("<<Users>>/Library/Containers/fr.handbrake.HandBrakeXPCService")
+      appFiles+=("<<Users>>/Library/Containers/fr.handbrake.sparkle-project.DownloaderService")
+      ;;
