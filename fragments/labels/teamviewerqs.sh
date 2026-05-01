0e4739275eaa102fb0158e9474266868bc118cdd
Sander Schram <macbofh@gmail.com>
2026-05-01 11:44:54 +0200

    Create teamviewerqs.sh

---
+++ fragments/labels/teamviewerqs.sh
@@ -0,0 +1,7 @@
+teamviewerqs)
+      appTitle="TeamViewerQS"
+      appProcesses+=("TeamViewerQS")
+      appFiles+=("/Applications/TeamViewerQS.app")
+      appFiles+=("<<Users>>/Library/Application Support/TeamViewerQS/")
+      appFiles+=("<<Users>>/Library/Preferences/com.teamviewer.TeamViewerQS.plist")
+      ;;
