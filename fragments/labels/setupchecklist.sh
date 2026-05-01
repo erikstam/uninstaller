322605f5896a05620364154568671462c3cae1e7
Sander Schram <macbofh@gmail.com>
2026-05-01 12:43:52 +0200

    Create setupchecklist.sh

---
+++ fragments/labels/setupchecklist.sh
@@ -0,0 +1,12 @@
+setupchecklist)
+      appTitle="Setup Checklist"
+      appProcesses+=("Setup Checklist" "setupchecklist" "Welcome")
+      appFiles+=("/Applications/Setup Checklist.app")
+      appFiles+=("/usr/local/bin/setupchecklist")
+      appFiles+=("<<Users>>/Library/Caches/com.jamf.setupchecklist")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.jamf.setupchecklist")
+      appFiles+=("<<Users>>/Library/Preferences/com.jamf.setup.welcome.plist")
+      appFiles+=("<<Users>>/Library/Preferences/com.jamf.setupchecklist.plist")
+      appLaunchAgents+=("/Library/LaunchAgents/com.jamf.setupchecklist.plist")
+      appReceipts+=("com.jamf.setupchecklist")
+      ;;
