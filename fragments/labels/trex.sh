b0d7eb8a445004e8c93a04788b91e8f016023287
Sander Schram <macbofh@gmail.com>
2026-05-01 11:02:53 +0200

    New Trex label

---
+++ fragments/labels/trex.sh
@@ -0,0 +1,13 @@
+trex)
+      appTitle="TRex"
+      appProcesses+=("TRex")
+      appFiles+=("/Applications/TRex.app")
+      appFiles+=("<<Users>>/Library/Application Support/TRex")
+      appFiles+=("<<Users>>/Library/Caches/com.ameba.TRex")
+      appFiles+=("<<Users>>/Library/Containers/com.ameba.TRex")
+      appFiles+=("<<Users>>/Library/Group Containers/X93LWC49WV.TRex.preferences")
+      appFiles+=("<<Users>>/Library/WebKit/com.ameba.TRex")
+      appFiles+=("<<Users>>/Library/HTTPStorages/com.ameba.TRex")
+      appFiles+=("<<Users>>/Library/Application Scripts/com.ameba.TRex")
+      appFiles+=("<<Users>>/Library/Preferences/com.ameba.TRex.plist")
+      ;;
