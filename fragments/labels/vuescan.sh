189f8ef1ef17a25560e57372f3fb5786ea177958
P-M Lejon <15982689+pmex@users.noreply.github.com>
2026-06-08 12:42:40 +0200

    Add VueScan

---
+++ fragments/labels/vuescan.sh
@@ -0,0 +1,7 @@
+vuescan)
+      appTitle="VueWcan"
+      appProcesses+=("VueScan")
+      appFiles+=("/Applications/VueScan.app")
+      appFiles+=("<<Users>>/Pictures/VueScan/vuescan.ini")
+      appFiles+=("<<Users>>/Pictures/VueScan/vuescan.log")
+      ;;
